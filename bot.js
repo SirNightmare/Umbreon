269c3a898fe5aefc29842c976198dcff83005904
SirNightmare <38385282+SirNightmare@users.noreply.github.com>
2018-07-09 18:28:07 +0100

    Add files via upload

--- bot.js
+++ bot.js
@@ -3,10 +3,12 @@ const commando = require("discord.js-commando");
 let prefix = "U/";
 const bot = new commando.CommandoClient({
     commandPrefix: "U/",
-    owner: "360908660727087107, 428371223043506182"
+    owner: "360908660727087107", 
+    owner: "428371223043506182"
 });
 
 bot.registry.registerGroup(`fun`, `Fun`);
+bot.registry.registerGroup(`useful`, `Useful`);
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
@@ -18,4 +20,4 @@ bot.on("ready", async () => {
     console.log(bot.commands);
     bot.user.setPresence({ game: { name: `My Siblings | U/help for help`, url: 'https://twitch.tv/monstercat', type: 1 } });
 });
-bot.login("Bot_Token_Here");
+bot.login("NDY1NTc3MzcyNDA2NTEzNjc4.DiP5Qg.BXNdnhgMSavdW08l0TCKRLvZ9O8");
